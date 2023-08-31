770197591408192f68aea39982922360390b4751
dhirajkashyap <65522950+dhirajkashyap@users.noreply.github.com>
2023-08-31 15:00:42 +0000

    Trial 1

--- go_lang/hello_world_3.go
+++ go_lang/hello_world_3.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+const (
+	c1 = iota
+	c2 = iota
+	c3 = iota
+)
+
+const (
+	c4 = iota
+	c5
+	c6
+	c7
+)
+
 func main() {
 	var i int
 	fmt.Printf("i = %d\n", i)
@@ -9,4 +22,6 @@ func main() {
 	fmt.Println("Hello Gophers!")
 	const name, age = "Dhiraj", 45
 	fmt.Printf("Name %s is %d years old i %d\n", name, age, i)
+	fmt.Println(c1, c2, c3)
+	fmt.Println(c4, c5, c6, c7)
 }
