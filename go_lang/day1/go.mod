9b9e98e4c12661638819b7eb85ed583774a7e3b5
dhirajkashyap <65522950+dhirajkashyap@users.noreply.github.com>
2023-09-19 15:00:09 +0000

    Second

--- go_lang/day1/go.mod
+++ go_lang/day1/go.mod
@@ -1,3 +1,5 @@
 module day1_try1
 
 go 1.21.0
+
+require github.com/dhirajkashyap/Puppy v0.0.0-20230914151739-3f3dbb0fb767 // indirect
--- go_lang/day1/main.go
+++ go_lang/day1/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/dhirajkashyap/Puppy"
 )
 
-func main () {
+func main() {
 	var x int = 23
-	fmt.Printf("value of x is %v, type is %T\n", x,x)
+	fmt.Printf("value of x is %v, type is %T\n", x, x)
+
+	s1 := puppy.Bark()
+	fmt.Printf("Hello says %s \n", s1)
+	s2 := puppy.Barks()
+	fmt.Printf("Hello twice says %s\n", s2)
+
 }
