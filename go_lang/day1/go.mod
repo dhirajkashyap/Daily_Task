fb790ebe4c6de1b74ef3ca1047f775ced648b919
dhirajkashyap <65522950+dhirajkashyap@users.noreply.github.com>
2023-09-26 15:19:09 +0000

    trial

--- go_lang/day1/go.mod
+++ go_lang/day1/go.mod
@@ -2,4 +2,6 @@ module day1_try1
 
 go 1.21.0
 
-require github.com/dhirajkashyap/Puppy v0.0.0-20230914151739-3f3dbb0fb767
+require github.com/dhirajkashyap/Puppy v0.0.0-20230921152922-9505057d7724
+
+require github.com/dhirajkashyap/Dog v0.0.0-20230921152450-10d127b69f64 // indirect
--- go_lang/day1/main.go
+++ go_lang/day1/main.go
@@ -10,8 +10,11 @@ func main() {
 	fmt.Printf("value of x is %v, type is %T\n", x, x)
 
 	s1 := puppy.Bark()
-	fmt.Printf("Hello says %s \n", s1)
+	fmt.Printf("Hello says Puppy %s \n", s1)
 	s2 := puppy.Barks()
-	fmt.Printf("Hello twice says %s\n", s2)
-
+	fmt.Printf("Hello twice says Puppy %s\n", s2)
+	s3 := puppy.Barkz()
+	fmt.Printf("Hello says Puppy, I got a new Barkz %s\n", s3)
+	s4 := puppy.BigBark()
+	fmt.Printf("Hello says Puppy - When am big, I gonna say %s\n", s4)
 }
