7d4e07e88e58de4bf84a8d648ef65cedebfb9bea
dhirajkashyap <65522950+dhirajkashyap@users.noreply.github.com>
2023-10-10 15:24:14 +0000

    day2

--- go_lang/day2/main.go
+++ go_lang/day2/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"math/rand"
 )
-
+func init() {
+	fmt.Println("This is during initialization.")
+}
 func main() {
 	x := rand.Intn(250)
 	fmt.Printf("Random value is %d\n", x)
@@ -15,5 +17,32 @@ func main() {
 		fmt.Println("Between 101 and 200")
 	} else {
 		fmt.Println("Between 201 and 250")
+	}
+
+	switch {
+	case x <= 100 :
+		fmt.Println ("Case between 0 and 100")
+		break;
+	case x<=200:
+		fmt.Println("Case Between 101 and 200")
+	default:
+		fmt.Println("Case Between 201 and 500, or maybe more")
+	}
+	x = rand.Intn(10)
+	y := rand.Intn(10)
+	fmt.Printf("x = %d, y = %d\n", x, y)
+
+	if ((x < 4) && (y < 4)) {
+		fmt.Println ("x and y are less than 4")
+	}
+	if ((x > 6) && (y > 6))	{
+		fmt.Println("x and y are greater than 6")
+	}
+	if ((x >= 4) && (x < 6)) {
+		fmt.Println("x is greater than equal to 4 and less than 6")
+	}
+
+	if (y != 5) {
+		fmt.Println ("y aint 5 yall")
 	}
 }
