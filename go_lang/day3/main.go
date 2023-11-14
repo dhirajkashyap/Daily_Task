febc4181d5e08d319ccc772f468745bd6dd4b2a7
dhirajkashyap <65522950+dhirajkashyap@users.noreply.github.com>
2023-11-14 16:53:35 +0000

    Done with Day3

--- go_lang/day3/main.go
+++ go_lang/day3/main.go
@@ -2,11 +2,18 @@ package main
 
 import "fmt"
 
+type person struct {
+	first string
+	last  string
+	age   int
+	favIC string
+}
+
 func main() {
 	m := make(map[string][]string)
-	m[`Bond_James`] = []string{`Shaken, Not stirred`, `martinis`, `fast cars`}
-	m[`Penny_Money`] = []string{`intelligence`, `literature`, `computer science`}
-	m[`No_Dr`] = []string{`Cats`, `icecreams`, `Mini Me`}
+	m[`Bond_James`] = []string{`Shaken, Not stirred`, `martinis`, `fast cars`, `Raspberry-Coconut`}
+	m[`Penny_Money`] = []string{`intelligence`, `literature`, `computer science`, `Mango-lime`}
+	m[`No_Dr`] = []string{`Cats`, `icecreams`, `Mini Me`, `Jalapeno-Orange`}
 
 	fmt.Printf("%#v\n", m)
 	for k, v := range m {
@@ -24,11 +31,41 @@ func main() {
 		}
 	}
 	fmt.Println("Adding new record now!")
-	m[`No_Dr_1`] = []string{`Dogs`, `Hot Dogs`, `Mini You`}
+	m[`No_Dr_1`] = []string{`Dogs`, `Hot Dogs`, `Mini You`, `Pineapple-Ghost-Pepper`}
 	for k, v := range m {
 		fmt.Println(k)
 		for l, v2 := range v {
 			fmt.Printf("%v\t%v\n", l, v2)
 		}
 	}
+
+	p1 := struct {
+		first string
+		last  string
+		age   int
+		favIC string
+	}{
+		first: "James",
+		last:  "Bond",
+		age:   32,
+		favIC: "Raspberry-Coconut",
+	}
+	p2 := person{
+		first: "Money",
+		last:  "Penny",
+		age:   25,
+		favIC: "Mango-Lime",
+	}
+	fmt.Printf("%T\n", p1)
+	fmt.Printf("%T\n", p2)
+	fmt.Printf("%#v\n", p1)
+	fmt.Printf("%#v\n", p2)
+
+	p2 = p1
+
+	fmt.Println("After initialization")
+	fmt.Printf("%T\n", p1)
+	fmt.Printf("%T\n", p2)
+	fmt.Printf("%#v\n", p1)
+	fmt.Printf("%#v\n", p2)
 }
