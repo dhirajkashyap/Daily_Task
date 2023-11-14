d1bb207056da7ade06fe2c64c67abe0b83a6adc3
dhirajkashyap <65522950+dhirajkashyap@users.noreply.github.com>
2023-11-14 16:14:05 +0000

    day3 done

--- go_lang/day3/main.go
+++ go_lang/day3/main.go
@@ -9,5 +9,26 @@ func main() {
 	m[`No_Dr`] = []string{`Cats`, `icecreams`, `Mini Me`}
 
 	fmt.Printf("%#v\n", m)
-
+	for k, v := range m {
+		fmt.Println(k)
+		for l, v2 := range v {
+			fmt.Printf("%v\t%v\n", l, v2)
+		}
+	}
+	fmt.Println("Now deleting record")
+	delete(m, "No_Dr")
+	for k, v := range m {
+		fmt.Println(k)
+		for l, v2 := range v {
+			fmt.Printf("%v\t%v\n", l, v2)
+		}
+	}
+	fmt.Println("Adding new record now!")
+	m[`No_Dr_1`] = []string{`Dogs`, `Hot Dogs`, `Mini You`}
+	for k, v := range m {
+		fmt.Println(k)
+		for l, v2 := range v {
+			fmt.Printf("%v\t%v\n", l, v2)
+		}
+	}
 }
