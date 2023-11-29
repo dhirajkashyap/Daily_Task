fddd3b1c3ff1638700083c202fe4e03a58dcca2a
dhirajkashyap <65522950+dhirajkashyap@users.noreply.github.com>
2023-11-29 16:01:13 +0000

    Done

--- go_lang/day4/main.go
+++ go_lang/day4/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+type Person struct {
+	first string
+	age   int
+}
+
 func main() {
 	//fmt.Println("Day4")
 	x := Sum([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
@@ -14,8 +19,20 @@ func main() {
 	z, s := TestBar(5)
 	fmt.Printf("Bar returned %d, and %s.\n", z, s)
 	fmt.Printf("Sum Foo1 = %d, Bar1 = %d\n", Foo1(x1...), Bar1(x1))
+	defer fmt.Println(3)
+	defer fmt.Println(2)
+	defer fmt.Println(1)
+	fmt.Println(0)
+	p1 := Person{
+		first: "James",
+		age:   33,
+	}
+	p1.Speak()
 }
 
+func (p Person) Speak() {
+	fmt.Println("My first name is", p.first, "and my age is", p.age)
+}
 func Sum(ii []int) int {
 	t := 0
 	for _, v := range ii {
